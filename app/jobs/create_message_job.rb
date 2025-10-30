6c2482702ba06c920ed6647f2032e3919edc8f4a
Mustafa omran <muostafa12@gmail.com>
2025-10-30 16:56:27 +0300

    fix message_count

--- app/jobs/create_message_job.rb
+++ app/jobs/create_message_job.rb
@@ -7,18 +7,20 @@ class CreateMessageJob < ApplicationJob
     # Create the message in the database
     message = chat.messages.create!(number: message_number, body: message_body)
 
-    # Index the message in Elasticsearch
-    Message.__elasticsearch__.index_document(message)
-
     # Atomically increment the counter
     Chat.increment_counter(:messages_count, chat_id)
+
+    # Index the message in Elasticsearch (best effort, don't fail job if this fails)
+    begin
+      Message.__elasticsearch__.index_document(message)
+    rescue => e
+      # Log Elasticsearch indexing errors but don't fail the job
+      Rails.logger.error("CreateMessageJob: Elasticsearch indexing failed for message #{message_number}: #{e.message}")
+      # Message is still persisted in MySQL, counter still incremented
+    end
   rescue ActiveRecord::RecordInvalid => e
     # Log error if message creation fails (e.g., duplicate number due to race condition)
     Rails.logger.error("CreateMessageJob failed for chat #{chat_id}, number #{message_number}: #{e.message}")
     raise
-  rescue => e
-    # Log Elasticsearch indexing errors but don't fail the job
-    Rails.logger.error("CreateMessageJob: Elasticsearch indexing failed for message #{message_number}: #{e.message}")
-    # Message is still persisted in MySQL, counter still incremented
   end
 end
